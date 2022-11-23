011d33c28918faf4f4d69b1ea9f6951e6baaa86c
Puskas David <puskas.david92@gmail.com>
2022-11-23 18:58:59 +0100

    First commit

---
+++ AoC_2022/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("AoC 2022");
+var sw = new Stopwatch();
+sw.Start();
+//DayXX.DayXX_Main();
+
+sw.Stop();
+Console.WriteLine($"Code run under {sw.ElapsedMilliseconds}ms");
+Console.ReadLine();
